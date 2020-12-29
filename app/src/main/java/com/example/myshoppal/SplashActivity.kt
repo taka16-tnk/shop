bede4aaf287d3379c652202d46cd021ab6610d1a
Takahiro Tanaka <t_tkhr@icloud.com>
2020-12-29 12:13:28 +0900

    Firebaseの設定 + Splash画面の作成

---
+++ app/src/main/java/com/example/myshoppal/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myshoppal
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                //launch the Main Activity
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()    //Call this when your activity is done and should be closed.
+            },
+            2500
+        )
+    }
+
+}
