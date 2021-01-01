9038943a935facbc21cae09b69f3a4025c68035c
Takahiro Tanaka <t_tkhr@icloud.com>
2021-01-01 15:37:47 +0900

    ログイン画面エラー修正

---
+++ app/src/main/java/com/example/myshoppal/activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myshoppal.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myshoppal.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
