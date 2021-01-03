2224df0540af6797e134176b9e345db35dad3c80
Takahiro Tanaka <t_tkhr@icloud.com>
2021-01-03 13:29:21 +0900

    ProgressBar表示処理を追加

--- app/src/main/java/com/example/myshoppal/activities/BaseActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/BaseActivity.kt
@@ -1,12 +1,18 @@
 package com.example.myshoppal.activities
 
+import android.app.Dialog
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.example.myshoppal.R
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.dialog_progress.*
 
 open class BaseActivity : AppCompatActivity() {
+
+    //Progress Dialog表示
+    private lateinit var mProgressDialog: Dialog
 
     //SnackBar表示
     fun showErrorSnackBar(message: String, errorMessage: Boolean) {
@@ -30,5 +36,27 @@ open class BaseActivity : AppCompatActivity() {
             )
         }
         snackBar.show()
+    }
+
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(this)
+
+        /**
+         * Set the screen content from a layout resource.
+         * The resource will be inflated, adding all top-level views to the screen.
+         */
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+
+        mProgressDialog.tv_progress_text.text = text
+
+        mProgressDialog.setCancelable(false)
+        mProgressDialog.setCanceledOnTouchOutside(false)
+
+        //Start the dialog and display it on screen.
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        mProgressDialog.dismiss()
     }
 }
--- app/src/main/java/com/example/myshoppal/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/RegisterActivity.kt
@@ -8,6 +8,10 @@ import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.example.myshoppal.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : BaseActivity() {
@@ -36,7 +40,7 @@ class RegisterActivity : BaseActivity() {
 
         //Registerボタン押下時の処理
         btn_register.setOnClickListener {
-            validateRegisterDetails()
+            registerUser()
         }
     }
 
@@ -93,11 +97,46 @@ class RegisterActivity : BaseActivity() {
                 false
             }
             else -> {
-                showErrorSnackBar(resources.getString(R.string.registery_successfull), false)
+                //showErrorSnackBar(resources.getString(R.string.registery_successfull), false)
                 true
             }
         }
     }
+
+    //ユーザー登録
+    private fun registerUser() {
+        //Check with validate function if the  entries are valid or not.
+        if (validateRegisterDetails()) {
+
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val email: String = et_email.text.toString().trim() { it <= ' ' }
+            val password: String = et_password.text.toString().trim { it <= ' ' }
+
+            //Create an instance and create a register a user with email and password.
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+
+                        hideProgressDialog()
+
+                        // If the registration is successfully done
+                        if (task.isSuccessful) {
+                            //Firebase registered user
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                            showErrorSnackBar(
+                                "You are registered successfully. Your user id is ${firebaseUser.uid}",
+                                false
+                            )
+                        } else {
+                            //If the registering is not successful then show error message.
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    })
+        }
+    }
+
 
 
 }
