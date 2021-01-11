246bd6531adda21f64c1b4b894da41f5aba7a268
Takahiro Tanaka <t_tkhr@icloud.com>
2021-01-11 15:01:49 +0900

    Create COMPLETE PROFILE screen

--- app/src/main/java/com/example/myshoppal/activities/LoginActivity.kt
+++ app/src/main/java/com/example/myshoppal/activities/LoginActivity.kt
@@ -50,8 +50,14 @@ class LoginActivity : BaseActivity(), View.OnClickListener {
         Log.i("Last Name: ", user.lastName)
         Log.i("Email: ", user.email)
 
-        // Redirect the user to Main Screen after log in.
-        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        if (user.profileCompleted == 0) {
+            //If the user profiles is incomplete then launch the UserProfileActivity.
+            val intent = Intent(this@LoginActivity, UserProfileActivity::class.java)
+            startActivity(intent)
+        } else {
+            // Redirect the user to Main Screen after log in.
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }
         finish()
     }
 
